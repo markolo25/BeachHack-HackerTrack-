61ba98a7db31a340619f354f01976cb127186a49
Jenny Wong <jew007@ucsd.edu>
2016-04-03 00:36:33 -0700

    Timer class implemented

--- DesktopClient/HTTimer.java
+++ DesktopClient/HTTimer.java
@@ -1,35 +1,75 @@
+/*
+ * Timer class to be used by HackerTracker desktop client
+ * By: Jenny Wong
+ */
+
 import java.util.Date;
 
 public class HTTimer {
 
-    int startTime;
-    int endTime;
-    int time;
 
+    /* Fields */
+    Date startTime;
+    Date endTime;
+    int time;
     boolean isGoing;
 
 
     /*
      * HTTime constructor
+     *
      * @param time - time spent on program already
      */
     public HTTimer(int time) {
 
+        this.startTime = null;
+        this.endTime = null;
         this.time = time;
         this.isGoing = false;
-
     }
 
-
-    public void go() {
-        Date currTime = new Date();
+    /*
+     * Collects start time
+     *
+     * @return true if successful, false if not
+     */
+    public boolean go() {
+        if (isGoing) {
+            return false;
+        }
+        this.isGoing = true;
+        this.startTime = new Date();
+        return true;
     }
 
-    public void stop() {
-
+    /*
+     * Collects stop time
+     *
+     * @return true if successful, false if not
+     */
+    public boolean stop() {
+        if (!isGoing) {
+            return false;
+        }
+        this.isGoing = false;
+        this.endTime = new Date();
+        return true;
     }
 
+    /*
+     * Calculates time elapsed
+     *
+     * @return int time elapsed
+     */
     public int getTime() {
-
+        if (isGoing) {
+            this.stop();
+            this.time = (int)((endTime.getTime() - startTime.getTime())/1000);
+            this.go();
+            return this.time;
+        }
+        
+        this.time = (int)((endTime.getTime() - startTime.getTime())/1000);
+        return this.time;
     }
 }
