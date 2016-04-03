e6079f43b33951d60283d652f805f91c76153a9f
Jenny Wong <jew007@ucsd.edu>
2016-04-02 21:58:51 -0700

    Desktop client

--- ChromeExtension/src/inject.js
+++ ChromeExtension/src/inject.js
@@ -5,9 +5,15 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
             active: true,
             lastFocusedWindow: true
         }, function(tabs) {
-        var tab = tabs[0];
-        var currUrl = tab.url;
-        alert(currUrl);
+
+            // Get the URL
+            var tab = tabs[0];
+            var currUrl = tab.url;
+            alert(currUrl);
+
+            // Get start time
+            var currTime = new Date();
+            alert (currTime);
         });
 
     }
---
+++ DesktopClient/HackerTracker.java
@@ -0,0 +1,106 @@
+/*
+ * Self-reported productivity timer
+ * By: Jenny Wong
+ */
+
+import javafx.application.*;
+import javafx.scene.control.*;
+import javafx.scene.*;
+import javafx.scene.paint.*;
+import javafx.scene.shape.*;
+import javafx.scene.layout.*;
+import javafx.stage.*;
+import javafx.event.*;
+import javafx.scene.input.*;
+import javafx.scene.text.*;
+import javafx.geometry.*;
+import java.util.*;
+import java.io.*;
+
+public class HackerTracker extends Application
+{
+
+    // Fill colors for GUI
+    private static final Color COLOR_EMPTY = Color.rgb(238, 228, 2);
+    private static final Color COLOR_2 = Color.rgb(255, 202, 195); 
+
+    private static final int FONT_SIZE = 35;
+    private static final int BIG_FONT_SIZE = 45;
+
+
+/* Method Name: start
+ * Purpose: Create graphical user interface. Overrides start in Application 
+ *          interface.
+ * Parameters: Stage primaryStage - JavaFX stage
+ * Return: void
+ */
+    @Override
+    public void start(Stage primaryStage)
+    {
+
+        // Create a grid pane to to hold everything & add to scrollpane so we
+        // can scroll if needed
+        GridPane pane = new GridPane();
+        ScrollPane scroll = new ScrollPane(pane);
+        scroll.setVbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+        scroll.setHbarPolicy(ScrollPane.ScrollBarPolicy.AS_NEEDED);
+
+        pane.setStyle("-fx-background-color: gold;"); // Background color
+        pane.setHgap(10); // Horizontal gap
+        pane.setVgap(10); // Vertical gap
+        pane.setPadding(new Insets(10, 10, 10, 10));
+
+        // Create and add title
+        Label nameLabel = new Label("HackerTracker");
+        nameLabel.setFont(Font.font("Arial", FontWeight.BOLD,
+                FontPosture.ITALIC, 15));
+        pane.add(nameLabel, 0, 0);
+
+        // Add first timer
+        int num_timers = 1;
+        Text txt = new Text("0:00");
+        Button startButton = new Button("Go");
+        Button stopButton = new Button("Stop");
+        pane.add( txt, 0, 1 );
+        pane.add( startButton, 1, 1);
+        pane.add( stopButton, 2, 1);
+
+        Button addTimer = new Button("+");
+        TextField timerName = new TextField();
+        pane.add( addTimer, 2, num_timers +1);
+        pane.add( timerName, 0, num_timers+1);
+
+        // Create and register add timer button handler
+        addTimer.setOnAction(e -> {
+
+
+            int row = pane.getRowIndex(addTimer);
+
+            pane.getChildren().remove(addTimer);
+            pane.getChildren().remove(timerName);
+
+            Text txttime = new Text("0:00");
+            Button start = new Button("Go");
+            Button stop = new Button("Stop");
+
+            pane.add( txttime, 0, row );
+            pane.add( start, 1, row );
+            pane.add( stop, 2, row );
+
+            row++;
+            pane.add( addTimer, 2, row );
+            pane.add( timerName, 0, row );
+
+        });
+
+        // Create a scene and place it in the stage
+        Scene scene = new Scene(scroll);
+        primaryStage.setTitle("Hacker Tracker Timer"); // Set the stage title
+        primaryStage.setScene(scene); // Place the scene in the stage
+        primaryStage.show(); // Display the stage
+
+        scroll.requestFocus();
+    }
+
+
+ }
