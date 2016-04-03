a0f1a1dba8635d967940607da4de781dfd991e1c
Mark Mendoza <markolo25@gmail.com>
2016-04-03 04:42:45 -0700

    started messing with xmlhttp maybe it'll work :P

--- ChromeExtension/src/inject.js
+++ ChromeExtension/src/inject.js
@@ -9,11 +9,19 @@ chrome.tabs.onUpdated.addListener(function(tabId, changeInfo) {
             // Get the URL
             var tab = tabs[0];
             var currUrl = tab.url;
-            alert(currUrl);
+            //alert(currUrl);
 
             // Get start time
             var currTime = new Date();
-            alert (currTime);
+            //alert(currTime);
+
+
+
+            var url = "ec2-54-183-149-25.us-west-1.compute.amazonaws.com:3000";
+            var params = currUrl + " " + currTime;
+            xmlhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+            xmlhttp.open("POST", url, true);
+            xmlhttp.send(params);
         });
 
     }
